0804961ef495198a092407262714dcbdacff4a8c
ERLMKea <erlm@kea.dk>
2022-09-01 09:48:19 +0200

    function with one argument

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,27 @@ import java.util.stream.Stream;
 
 public class Main {
 
+    public interface ZeroArgument {
+        public void myFunc();
+    }
+
+    public static void printZero(ZeroArgument zf) {
+        zf.myFunc();
+    }
+
+    public interface OneArgument {
+        public void myFunc(String str);
+    }
+
+    public static void printOne(OneArgument of1, String str) {
+        of1.myFunc(str);
+    }
+
     public static void main(String[] args) {
+        ZeroArgument z = () -> System.out.println("Hello world");
+        printZero(z);
+        OneArgument o = (str) -> System.out.println(str);
+        o.myFunc("hej du der");
+        printOne(o, "i am functionel with 1 argument");
     }
 }
